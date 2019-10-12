329ffe4cb7ab0d88de3f6be61e6ae4c91fa32748
Alexandra Morozoff <a.j.morozoff@gmail.com>
2019-10-11 23:49:38 -0400

    basic functionality

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,42 @@
 class FamilyTree {
-  constructor () {}
+  constructor (val) {
+    if (!val || (typeof val != 'string')) {
+      throw ('value required');
+    }
+    this.value = val;
+    this.children = [];
+  }
+
+  insert(val) {
+    let child = new FamilyTree(val);
+    this.children.push(child);
+  }
+
+  familySize() {
+    return 1 + this.children.length;
+  }
+
+  findMember(member) {
+    if (this.value === member) {
+      return this;
+    }
+    return this.children.filter(child => child.value === member)[0];
+  }
+
+  log() {
+
+    const logHelper = (tree, prefix) => {
+        let subString = prefix + ' ' + tree.value + '\n';
+        tree.children.forEach(child => {
+          subString += logHelper(child, prefix + `--`);
+        })
+        return subString;
+    }
+
+    return logHelper(this, `--`);
+
+  }
+
 }
 
 module.exports = FamilyTree;
